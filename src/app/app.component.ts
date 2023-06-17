6504c9611969211055fc9afc264f1139e2ac96c7
Saurabh <saurabh.uthaman786@gmail.com>
2023-06-17 11:25:31 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,64 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'e-comm-project';
+  url:string ='assets/watch1.jpg'
+  url1:string ='assets/product2.png'
+
+  changeimage(event:any){
+    this.url=event.target.src;
+  }
+  changeimage1(event:any){
+    this.url1=event.target.src;
+  }
+  clothOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: false,
+    dots: false,
+    autoplay:true,
+    autoplayTimeout:1000,
+    navSpeed: 700,
+    navText: ['', ''],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 2
+      },
+      740: {
+        items: 3
+      },
+      940: {
+        items: 5
+      }
+    },
+    nav: true
+  }
+
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: false,
+    dots: false,
+    navSpeed: 700,
+    navText: ['<i class="fa fa-caret-left">', '<i class="fa fa-caret-right">'],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 2
+      },
+      740: {
+        items: 3
+      },
+      940: {
+        items: 5
+      }
+    },
+    nav: true
+  }
 }
